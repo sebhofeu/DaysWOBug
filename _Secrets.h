675d74890d99f2d8dd77c0ba02347e7d8bb5487a
Sebastian Hoffmann <sho@fp.de>
2025-10-06 14:54:56 +0200

    Added Jira-API-Token doc

--- _Secrets.h
+++ _Secrets.h
@@ -7,4 +7,4 @@ ToDo: Rename to Secrets.h and add correct information below
 #define STAPSK "wlan pw"
 #endif
 
-static const String API_TOKEN = "jira api token"; 
+static const String API_TOKEN = "jira api token"; // = Base64("usermail:" + Jira-API-Token) Generat API-Token: Jira/Account Settings/Security/Create API Token
